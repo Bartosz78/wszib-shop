d3f097a25549e57921f2683ae4249b397546f718
Bartosz78 <35394112+Bartosz78@users.noreply.github.com>
2018-01-13 19:04:29 +0100

    Init

---
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Domain;
+using Shop.Core.Repositories;
+using Shop.Core.Services;
+using Shop.Web.Models;
+using System.Linq;
+
+namespace Shop.Web.Controllers
+{
+    [Route("products")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = _productService
+                .GetAll()
+                .Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Category = p.Category,
+                    Price = p.Price
+                });
+
+            return View(products);
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            var viewModel = new AddProductViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(AddProductViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            _productService.Add(viewModel.Name, viewModel.Category, viewModel.Price);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ src/Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Models
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name was not provided.")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+    }
+}
